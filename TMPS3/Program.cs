fbbf4a2215d71820442bb92f455a7be0093f17b0
Alin Prisacaru <alin.prisacaru2001@gmail.com>
2023-06-08 06:13:46 +0300

    Add project files.

---
+++ TMPS3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TMPS3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
